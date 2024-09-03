16235a579c42be8325b864aeb959a6a0d5c0f395
Lucia Craven Garat <luciacraven93@gmail.com>
2024-09-03 13:37:29 +0200

    creando crud pets con db.json y axios

--- Veterinaria/src/utils/index.js
+++ Veterinaria/src/utils/index.js
@@ -1,27 +1,65 @@
-const urlUsers = "http://localhost:3000/users"
-const urlPets = "http://localhost:3000/pets"
-const urlTurnos = "http://localhost:3000/turnos"
+const urlUsers = "http://localhost:3000/users";
+const urlPets = "http://localhost:3000/pets";
+const urlTurnos = "http://localhost:3000/turnos";
 import axios from "axios";
 
 //----------------CRUD PETS-----------
 
-export const getPets = async()=> {
+export const getPets = async () => {
+  try {
+    let pets = await fetch(urlPets);
+    return pets.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updatePet = async (id, obj) => {
+  console.log(id);
+  console.log(obj);
+
+  try {
+    let pet = await axios.put(`${urlPets}/${id}`, obj);
+    return pet;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// updatePet(2, {
+//   id: 2,
+//   name: "loli",
+//   specie: "can",
+//   raza: "indefinida",
+// });
+
+export const createPet = async (obj) => {
+  try {
+    let pet = await axios.post(`${urlPets}`, obj);
+    return pet;
+  } catch (error) {
+    console.log(error);
+    
+  }
+};
+
+// createPet({
+//   id: 3,
+//   name: "Micha",
+//   specie: "can",
+//   raza: "caniche",
+// });
+
+export const deletePet = async (id) => {
     try {
-        let pets = await fetch(urlPets)
-        return pets.json()
+      let pet = await axios.delete(`${urlPets}/${id}`);
+      return pet
     } catch (error) {
         console.log(error);
-        
     }
-}
+  };
 
-export const updatePet = async ()=>{
-    try {
-        let pet = await axios.put()
-    } catch (error) {
-        
-    }
-}
+  //deletePet("3");
 
 
 
@@ -29,8 +67,8 @@ export const updatePet = async ()=>{
 
 // const getUsers = async ()=> {
 //     try {
-        
+
 //     } catch (error) {
-        
+
 //     }
 // }
